5d1f775533e09000320dbdd9ff0806d4bf848280
ghartemann <g.hartemann@gmail.com>
2024-07-02 19:27:02 +0200

    ajout logique exceptions

--- exceptions.js
+++ exceptions.js
@@ -1,27 +1,39 @@
 export default {
     pluriel: {
-        // Les noms terminés par -eu ou -au au singulier prennent un -x au pluriel
+        // Les noms terminés par -eu ou -au au singulier prennent un -x au pluriel, sauf
         'pneu': 'pneus',
         'landau': 'landaus',
         'bleu': 'bleus',
         'émeu': 'émeus',
         'lieu': 'lieus', // euh celui-là c'est la merde
 
-        // Les noms terminés par -ou au singulier prennent un -s au pluriel
-        "hibou": "hiboux",
-        'chou': "choux",
-        'caillou': "cailloux",
+        // Les noms terminés par -ou au singulier prennent un -s au pluriel, sauf
         'bijou': "bijoux",
+        'caillou': "cailloux",
+        'chou': "choux",
         'genou': "genoux",
-        'pou': "poux",
+        'hibou': "hiboux",
         'joujou': "joujoux",
+        'pou': "poux",
 
-        // Les noms terminés par -al au singulier ont un pluriel en -aux
+        // Les noms terminés par -al au singulier ont un pluriel en -aux, sauf
         'bal': "bals",
         'chacal': "chacals",
         'festival': "festivals",
         'récital': "récitals",
         'cheval': "chevaux",
         'étal': "étals",
+
+        // Les noms terminés par -ail au singulier ont un pluriel en -aux, sauf
+        'bail': "baux",
+        'corail': "coraux",
+        'émail': "émaux",
+        'soupirail': "soupiraux",
+        'travail': "travaux",
+        'fermail': "fermaux",
+        'gemmail': "gemmaux",
+        'ventail': "ventaux",
+        'vantail': "vantaux",
+        'vitrail': "vitraux"
     }
 }
--- grammaire.ts
+++ grammaire.ts
@@ -6,6 +6,30 @@ export class Grammaire {
         if (typeof quantite !== 'number') throw new TypeError('La quantité doit être un nombre (int)');
         if (quantite <= 0) throw new RangeError('La quantité doit être strictement supérieure à 0');
 
+        const last2Chars = mot.slice(-2);
+
+        if (last2Chars === 'au' || last2Chars === 'eu') {
+            return quantite > 1 ? mot + 'x' : mot;
+        }
+
+        if (last2Chars === 'al') {
+            if (this.isException(mot)) return this.getException(mot);
+
+            return quantite > 1 ? mot.slice(0, -2) + 'aux' : mot;
+        }
+
         return quantite > 1 ? mot + 's' : mot;
+    }
+
+    static isException(mot: string): boolean {
+        if (typeof mot !== 'string') throw new TypeError('Le mot doit être une chaîne de caractères');
+
+        return exceptions.pluriel.includes(mot) || exceptions.pluriel.includes(mot);
+    }
+
+    static getException(mot: string): string {
+        if (typeof mot !== 'string') throw new TypeError('Le mot doit être une chaîne de caractères');
+
+        return exceptions.pluriel[mot];
     }
 }
