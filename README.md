2de0c9833bb51c9b6769e5af37a1d0ad744ca308
Guillaume HARTEMANN-PIOLLET <ghartemann@com-company.fr>
2024-07-03 14:03:01 +0200

    update exceptions + readme

--- README.md
+++ README.md
@@ -19,15 +19,75 @@ npm install grammaire
 
 ## Utilisation
 
-```javascript
-import { pluralise } from 'grammaire';
+### pluralise()
+Permet de transformer un mot au pluriel en fonction d'un nombre donné.
 
-console.log(pluralise('pomme', 2)); // pommes
-console.log(pluralise('cheval', 2)); // chevaux
-
-// Pour indiquer un pluriel spécifique et/ou surcharger le pluriel par défaut
-console.log(pluralise('cheval', 2, 'chevals')); // chevals
+```typescript
+function pluralise(mot: string, quantite: number, pluriel: string = null): string {}
 ```
+
+Exemples :
+```javascript
+Grammaire.pluralise('pomme', 2); // pommes
+Grammaire.pluralise('cheval', 2); // chevaux
+```
+
+Il est possible de surcharger la méthode en passant un pluriel en paramètre.
+```javascript
+Grammaire.pluralise('cheval', 2, 'chevals'); // chevals
+```
+
+### multiPluralise()
+Pluralise plusieurs mots successifs en fonction de la quantité donnée
+```typescript
+Grammaire.multiPluralise(['chat', 'orange'], 1); // chat orange
+Grammaire.multiPluralise(['cheval', 'noir'], 2); // chevaux noirs
+Grammaire.multiPluralise(['serval', 'rouge'], 2); // servals rouges
+```
+Attention : ne fournir que les mots à pluraliser
+```typescript
+Grammaire.multiPluralise(['cheval', 'pur-sang'], 2); // chevaux pur-sangs => faux
+```
+
+### isException()
+Permet de vérifier si un mot est une exception (si son pluriel diffère de la règle).
+
+```typescript
+function isException(mot: string): boolean {}
+```
+
+Exemples :
+```javascript
+Grammaire.isException('chat'); // false
+Grammaire.isException('serval'); // true
+```
+
+
+### getException()
+Permet de récupérer le pluriel d'un mot exception.
+
+```typescript
+function getException(mot: string): string {}
+```
+
+Exemples :
+```javascript
+Grammaire.getException('serval'); // servals
+Grammaire.getException('chat'); // Error: Le mot n'est pas une exception
+```
+
+## Tests
+```bash
+yarn test
+```
+
+Cependant les tests sont pétés actuellement.
+
+## Contribuer
+Pour contribuer, il suffit de cloner le projet et de créer une pull request.
+
+## License
+[ISC](https://opensource.org/licenses/ISC)
 
 ## Ressources
 [Pluriels irréguliers en français](https://fr.wiktionary.org/wiki/Annexe:Pluriels_irr%C3%A9guliers_en_fran%C3%A7ais)
--- grammaire.ts
+++ grammaire.ts
@@ -1,4 +1,4 @@
-import exceptions from "./exceptions";
+import ExceptionsPluriel from "./ExceptionsPluriel";
 import Assess from "./assess";
 
 export default class Grammaire {
@@ -26,8 +26,8 @@ export default class Grammaire {
 
         // mots en -s, -x, -z
         if (lastChar === 's' || lastChar === 'x' || lastChar === 'z') {
-            if (this.isException(mot, 'pluriel')) {
-                return this.getException(mot, 'pluriel');
+            if (this.isException(mot)) {
+                return this.getException(mot);
             }
 
             return mot;
@@ -37,8 +37,8 @@ export default class Grammaire {
 
         // mots en -eu ou -au
         if (last2Chars === 'au' || last2Chars === 'eu') {
-            if (this.isException(mot, 'pluriel')) {
-                return this.getException(mot, 'pluriel');
+            if (this.isException(mot)) {
+                return this.getException(mot);
             }
 
             return mot + 'x';
@@ -46,8 +46,8 @@ export default class Grammaire {
 
         // mots en -ou
         if (last2Chars === 'ou') {
-            if (this.isException(mot, 'pluriel')) {
-                return this.getException(mot, 'pluriel');
+            if (this.isException(mot)) {
+                return this.getException(mot);
             }
 
             return mot + 's';
@@ -55,8 +55,8 @@ export default class Grammaire {
 
         // mots en -al
         if (last2Chars === 'al') {
-            if (this.isException(mot, 'pluriel')) {
-                return this.getException(mot, 'pluriel');
+            if (this.isException(mot)) {
+                return this.getException(mot);
             }
 
             return mot.slice(0, -2) + 'aux';
@@ -66,13 +66,31 @@ export default class Grammaire {
         return mot + 's';
     }
 
+    /** Pluralise plusieurs mots successifs en fonction de la quantité donnée<br>
+     * ```typescript
+     * Grammaire.multiPluralise(['chat', 'orange'], 1); // chat orange
+     * Grammaire.multiPluralise(['cheval', 'noir'], 2); // chevaux noirs
+     * Grammaire.multiPluralise(['serval', 'rouge'], 2); // servals rouges
+     * ```
+     * Attention : ne fournir que les mots à pluraliser<br>
+     * ```typescript
+     * Grammaire.multiPluralise(['cheval', 'pur-sang'], 2); // chevaux pur-sangs => faux
+     * ```*/
+    static multiPluralise(mots: string[], quantite: number): string {
+        mots.forEach((mot, index) => {
+            mots[index] = this.pluralise(mot, quantite);
+        });
+
+        return mots.join(' ');
+    }
+
     /** Détermine si le mot figure dans la liste des exceptions<br>
      * ```typescript
      * Grammaire.isException('chat'); // false
      * Grammaire.isException('serval'); // true
      * ```*/
-    static isException(mot: string, type: string): boolean {
-        return Object.keys(exceptions[type]).includes(mot);
+    static isException(mot: string): boolean {
+        return Object.keys(ExceptionsPluriel).includes(mot);
     }
 
     /** Renvoie le pluriel du mot SI celui-ci est une exception<br>
@@ -80,9 +98,11 @@ export default class Grammaire {
      * Grammaire.getException('serval'); // servals
      * Grammaire.getException('chat'); // Error: Le mot n'est pas une exception
      * ```*/
-    static getException(mot: string, type: string): string {
-        if (!this.isException(mot, type)) throw new Error('Le mot n\'est pas une exception');
+    static getException(mot: string): string {
+        if (!this.isException(mot)) {
+            throw new Error('Le mot n\'est pas une exception');
+        }
 
-        return exceptions[type][mot];
+        return ExceptionsPluriel[mot];
     }
 }
