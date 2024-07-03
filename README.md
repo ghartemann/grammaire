df31ee74fcde79e097dcdab76d8ef986392a9c91
ghartemann <g.hartemann@gmail.com>
2024-07-03 08:08:52 +0200

    update readme + logique + ajout tests

---
+++ README.md
@@ -0,0 +1,33 @@
+# Grammaire
+
+Notre chère langue française est complexe et bourrée de cas particuliers, il est ainsi parfois difficile de gérer les différentes formes
+que peuvent prendre les mots en fonction de leur genre ou nombre.
+
+Ce package a pour but de simplifier les manipulations de chaînes de caractères en proposant diverses fonctions.
+
+## Installation
+
+#### Yarn
+```bash
+yarn add grammaire
+```
+
+#### NPM
+```bash
+npm install grammaire
+```
+
+## Utilisation
+
+```javascript
+import { pluralise } from 'grammaire';
+
+console.log(pluralise('pomme', 2)); // pommes
+console.log(pluralise('cheval', 2)); // chevaux
+
+// Pour indiquer un pluriel spécifique et/ou surcharger le pluriel par défaut
+console.log(pluralise(['cheval', 'chevals'], 2)); // chevals
+```
+
+## Ressources
+[Pluriels irréguliers en français](https://fr.wiktionary.org/wiki/Annexe:Pluriels_irr%C3%A9guliers_en_fran%C3%A7ais)
--- grammaire.ts
+++ grammaire.ts
@@ -1,27 +1,42 @@
 import exceptions from "./exceptions";
 
 class Grammaire {
-    static pluraliser(mot: string, quantite: number): string {
+    /** Pluralise un mot en fonction de la quantité donnée<br>
+     * Exemple :<br>
+     * ```typescript
+     * Grammaire.pluraliser('chat', 1); // chat
+     * Grammaire.pluraliser('cheval', 2); // chevaux
+     * ```
+     */
+    static pluralise(mot: string, quantite: number): string {
         if (typeof mot !== 'string') throw new TypeError('Le mot doit être une chaîne de caractères');
         if (typeof quantite !== 'number') throw new TypeError('La quantité doit être un nombre (int)');
         if (quantite <= 0) throw new RangeError('La quantité doit être strictement supérieure à 0');
 
+        const lastChar = mot.slice(-1);
+
+        if (lastChar === 's' || lastChar === 'x' || lastChar === 'z') {
+            if (this.isException(mot, 'pluriel')) return this.getException(mot, 'pluriel');
+
+            return mot;
+        }
+
         const last2Chars = mot.slice(-2);
 
         if (last2Chars === 'au' || last2Chars === 'eu') {
-            if (this.isException(mot)) return this.getException(mot);
+            if (this.isException(mot, 'pluriel')) return this.getException(mot, 'pluriel');
 
             return quantite > 1 ? mot + 'x' : mot;
         }
 
         if (last2Chars === 'ou') {
-            if (this.isException(mot)) return this.getException(mot);
+            if (this.isException(mot, 'pluriel')) return this.getException(mot, 'pluriel');
 
             return quantite > 1 ? mot + 's' : mot;
         }
 
         if (last2Chars === 'al') {
-            if (this.isException(mot)) return this.getException(mot);
+            if (this.isException(mot, 'pluriel')) return this.getException(mot, 'pluriel');
 
             return quantite > 1 ? mot.slice(0, -2) + 'aux' : mot;
         }
@@ -29,16 +44,18 @@ class Grammaire {
         return quantite > 1 ? mot + 's' : mot;
     }
 
-    static isException(mot: string): boolean {
+    static isException(mot: string, type: string): boolean {
         if (typeof mot !== 'string') throw new TypeError('Le mot doit être une chaîne de caractères');
+        if (typeof type !== 'string') throw new TypeError('Le type doit être une chaîne de caractères');
 
-        return Object.keys(exceptions.pluriel).includes(mot) || Object.keys(exceptions.pluriel).includes(mot);
+        return Object.keys(exceptions[type]).includes(mot);
     }
 
-    static getException(mot: string): string {
+    static getException(mot: string, type: string): string {
         if (typeof mot !== 'string') throw new TypeError('Le mot doit être une chaîne de caractères');
+        if (typeof type !== 'string') throw new TypeError('Le type doit être une chaîne de caractères');
 
-        return exceptions.pluriel[mot];
+        return exceptions[type][mot];
     }
 }
 
